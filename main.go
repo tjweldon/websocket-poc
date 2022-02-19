1a6a1225b2dd0d95eb22944e9873251677745bdc
tjweldon <tomd90@gmail.com>
2022-02-19 17:09:36 +0000

    debug page setup in preparation for switch to frame buffering

--- main.go
+++ main.go
@@ -22,33 +22,14 @@ var addr = flag.String("addr", "localhost:8080", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
 
-func echo(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
-	}
-}
-
+// LPFloat serialises as a number represented to a fixed number
+// decimal places eg. 1.00
 type LPFloat struct {
 	Value  float64 // the actual value
 	Digits int     // the number of digits used in json
 }
 
+// MarshalJSON serialises the LPFloat Type
 func (l LPFloat) MarshalJSON() ([]byte, error) {
 	s := fmt.Sprintf("%.*f", l.Digits, l.Value)
 	return []byte(s), nil
@@ -60,45 +41,60 @@ type Coords struct {
 }
 
 func tick(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
+	// Upgrade the web request to a socket
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
 		return
 	}
-	defer c.Close()
-	ticker := time.NewTicker(time.Second / 60)
+
+	// Socket close on function return
+	defer conn.Close()
+
+	// Initiate Clock
+	ticker := time.NewTicker(time.Second / 70)
 	defer ticker.Stop()
+
+	// Signal to kill the socket
 	done := make(chan bool)
 	go func() {
 		time.Sleep(10 * time.Second)
 		done <- true
 	}()
+
+	// Stream loop
 	for {
 		select {
 		case <-done:
-			err = c.WriteMessage(websocket.TextMessage, []byte("Done!"))
+			// Send a farewell
+			err = conn.WriteMessage(websocket.TextMessage, []byte("Done!"))
 			return
 		case t := <-ticker.C:
+			// Calculate coordinates
 			timeFloat := float64(t.UnixMilli()%1000) / 1e3
 			coords := Coords{
-				X: LPFloat{Value: 10 * math.Cos(2*math.Pi*timeFloat), Digits: 2},
-				Y: LPFloat{Value: 10 * math.Sin(2*math.Pi*timeFloat), Digits: 2},
+				X: LPFloat{Value: 100*math.Cos(2*math.Pi*timeFloat) + 200, Digits: 2},
+				Y: LPFloat{Value: 100*math.Sin(2*math.Pi*timeFloat) + 200, Digits: 2},
 			}
+
+			// JSON serialise
 			rawJson, err := json.Marshal(coords)
 			if err != nil {
 				panic(err)
 			}
-			err = c.WriteMessage(websocket.TextMessage, rawJson)
+
+			// SEND IT LADS, YEET
+			err = conn.WriteMessage(websocket.TextMessage, rawJson)
 		}
 	}
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	homeTemplate.Execute(w, "ws://"+r.Host+"/tick")
+func index(w http.ResponseWriter, r *http.Request) {
+	otherTemplate.Execute(w, "ws://"+r.Host+"/tick")
 }
 
-func other(w http.ResponseWriter, r *http.Request) {
-	otherTemplate.Execute(w, "ws://"+r.Host+"/tick")
+func debug(w http.ResponseWriter, r *http.Request) {
+	debugTemplate.Execute(w, "ws://"+r.Host+"/tick")
 }
 
 func main() {
@@ -110,12 +106,13 @@ func main() {
 	http.Handle("/resources/", http.FileServer(http.Dir(".")))
 
 	// Sockets
-	//http.HandleFunc("/echo", echo)
 	http.HandleFunc("/tick", tick)
 
 	// Pages
-	//http.HandleFunc("/other", home)
-	http.HandleFunc("/", other)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/debug", debug)
+
+	// Start server
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
 
@@ -131,92 +128,6 @@ var otherTemplate = template.Must(
 	template.New("index").Parse(getFileText("./home.html")),
 )
 
-var homeTemplate = template.Must(template.New("").Parse(`
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<script>  
-window.addEventListener("load", function(evt) {
-
-    var output = document.getElementById("output");
-    var input = document.getElementById("input");
-    var ws;
-
-    var print = function(message) {
-        var d = document.createElement("div");
-        d.textContent = message;
-        output.innerHTML = message;
-        output.scroll(0, output.scrollHeight);
-    };
-
-    document.getElementById("open").onclick = function(evt) {
-        if (ws) {
-            return false;
-        }
-        ws = new WebSocket("{{.}}");
-        ws.onopen = function(evt) {
-            print("OPEN");
-        }
-        ws.onclose = function(evt) {
-            print("CLOSE");
-            ws = null;
-        }
-        ws.onmessage = function(evt) {
-            print("T: " + evt.data);
-        }
-        ws.onerror = function(evt) {
-            print("ERROR: " + evt.data);
-        }
-        return false;
-    };
-
-    document.getElementById("send").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        print("SEND: " + input.value);
-        ws.send(input.value);
-        return false;
-    };
-
-    document.getElementById("close").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        ws.close();
-        return false;
-    };
-
-});
-</script>
-</head>
-<body>
-<table>
-<tr>
-	<td width="20%">
-	<form>
-	<button id="open">Open</button>
-	<button id="close">Close</button>
-	</form>
-	</td>
-	<td valign="top" width="80%">
-	<p>Click "Open" to create a connection to the server, 
-	"Send" to send a message to the server and "Close" to close the connection. 
-	You can change the message and send multiple times.
-	<p>
-	</td>
-	</tr>
-<tr>
-	<td width="20%">
-	<div id="output" style="max-height: 70vh;overflow-y: scroll; font-family: 'Courier New', monospace; font-size: 10pt"></div>
-	</td>
-	<td width="80%">
-    <main></main>
-	</td>
-</tr>
-</table>
-
-</body>
-</html>
-`))
+var debugTemplate = template.Must(
+	template.New("index").Parse(getFileText("./debug.html")),
+)
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 const flock = [];
-
-let alignSlider, cohesionSlider, separationSlider;
+let frameBuffer = []
 
 const WIDTH = 1200;
 const HEIGHT = 600;
@@ -12,44 +11,38 @@ const SLIDER_SEP = 40;
 const setup = (s) => () => {
     s.createCanvas(WIDTH, HEIGHT);
     s.background(0);
-
-    var slider_cursor = { x: SLIDER_ROW_MARGIN, y: SLIDER_ROW };
-
-    separationSlider = s.createSlider(0, 5, 0, 0.1);
-    separationSlider.position(slider_cursor.x, slider_cursor.y);
-    slider_cursor.x += separationSlider.width + SLIDER_SEP;
-
-    cohesionSlider = s.createSlider(0, 5, 0, 0.1);
-    cohesionSlider.position(slider_cursor.x, slider_cursor.y);
-    slider_cursor.x += separationSlider.width + SLIDER_SEP;
-
-    alignSlider = s.createSlider(0, 5, 0, 0.1);
-    alignSlider.position(slider_cursor.x, slider_cursor.y);
-    slider_cursor.x += separationSlider.width + SLIDER_SEP;
 };
+
 
 let i = 0;
 const draw = (s) => () => {
     s.background(0);
-
-    s.strokeWeight(3);
-    s.textSize(14);
-    s.text(`Separation: ${separationSlider.value()}`, separationSlider.x + 20, separationSlider.y - 10);
-    s.text(`Cohesion: ${cohesionSlider.value()}`, cohesionSlider.x + 20, cohesionSlider.y - 10);
-    s.text(`Align: ${alignSlider.value()}`, alignSlider.x + 20, alignSlider.y - 10);
-    s.text(`BOID: ${flock[0]}`, 0, 50);
-
-    for (let boid of flock) {
-        boid.flock(s, flock);
-        boid.update(s);
-        boid.edges(s);
-        boid.show(s);
-    }
+    show(s);
 };
 
 let sketch = (s) => {
     s.setup = setup(s);
     s.draw = draw(s);
 };
+
+const show = (s) => {
+    if (frameBuffer.length < 1) {
+        return
+    }
+    let position;
+    s.background(0)
+    let frame = frameBuffer.shift();
+    if (frame.includes("{")) {
+        position = JSON.parse(frame);
+    } else {
+        return;
+    }
+
+
+    s.strokeWeight(8);
+    s.stroke(255);
+    s.point(position.x, position.y);
+}
+
 
 const sketchInstance = new p5(sketch);
